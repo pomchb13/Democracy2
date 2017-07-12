a803cb7e9e13ae7f6d3b9cc4d7025d303775231f
harweb13 <harewb13@htl-kanidorf.ac.at>
2017-07-12 19:29:47 +0200

    Try Autoclose

---
+++ web/js/CloseWin.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Ewald on 12.07.2017.
+ */
+/**
+ * function loaded() {
+    alert("Beep!");
+    self.setTimeout(CloseMe, 500);
+}
+
+ function CloseMe() {
+    self.close();
+}
+ */
