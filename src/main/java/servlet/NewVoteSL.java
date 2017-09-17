30caee8aa3c08ba803236d6c679c13c9688435e9
ganleb13 <ganleb13@htl-kaindorf.ac.at>
2017-09-17 19:10:48 +0200

    WebServlet Annotation

--- src/main/java/servlet/NewVoteSL.java
+++ src/main/java/servlet/NewVoteSL.java
@@ -5,6 +5,7 @@ import util.servletUtil;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,10 +14,11 @@ import java.io.IOException;
 /**
  * Created by Ewald on 17.08.2017.
  */
+@WebServlet(urlPatterns = {"/NewVoteSL"})
 public class NewVoteSL extends HttpServlet {
+
     @Override
     public void init(ServletConfig config) throws ServletException {
-
         super.init(config);
     }
 
