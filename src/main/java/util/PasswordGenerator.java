5f7ee4c7aa32507043f23a09579bdd5244db428a
pomchb13 <pomchb13@htl-kaindorf.ac.at>
2018-02-18 13:50:12 +0100

    Added a PasswordGenerator

---
+++ src/main/java/util/PasswordGenerator.java
@@ -0,0 +1,40 @@
+package util;
+
+
+import java.security.SecureRandom;
+
+/**
+ * Created by Christoph Pommer on 18.02.2018
+ * Class to generate a Secure Password
+ */
+public class PasswordGenerator {
+
+
+
+
+    private static final SecureRandom secureRand = new SecureRandom();
+    /**
+     * Letters to generate the password
+     */
+    private static final String AVAILABLELETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+
+
+    /**
+     *
+     * Generates a password out of the Available Letters and the length with a securerandom object
+     * @param length of password
+     * @return the new password
+     */
+    public static String createPassword(int length)
+    {
+        String generatedPasswd="";
+        for(int i=0;i<length;i++)
+        {
+            int randInt = secureRand.nextInt(AVAILABLELETTERS.length());
+            generatedPasswd+= AVAILABLELETTERS.charAt(randInt);
+        }
+        return generatedPasswd;
+    }
+
+
+}
