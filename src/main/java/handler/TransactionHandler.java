fdac468cf0ffa6d76d87ed4acb92e7940b4851c5
Patrick Windegger <winpab13@htl-kaindorf.ac.at>
2018-03-10 16:02:58 +0100

    TransactionHandler JavaDoc

--- src/main/java/handler/TransactionHandler.java
+++ src/main/java/handler/TransactionHandler.java
@@ -6,15 +6,13 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.Transfer;
 import org.web3j.utils.Convert;
-import util.BlockchainUtil;
 
-import java.io.File;
 import java.math.BigDecimal;
 
 /**
  * Author:          Patrick Windegger
  * Created on:
- * Description:
+ * Description:     Class responsible for sending Ether from one account to another account
  */
 public class TransactionHandler {
 
@@ -22,12 +20,25 @@ public class TransactionHandler {
     private Credentials credentials;
 
 
+    /**
+     * Constructor for initializing the web3 Http service and credentials
+     *
+     * @param credentials of the admin
+     */
     public TransactionHandler(Credentials credentials) {
         web3 = Web3j.build(new HttpService());
         this.credentials = credentials;
     }
 
-    public void sendTransaction(Address to) throws Exception {
-        Transfer.sendFunds(web3, credentials, to.toString(), BigDecimal.valueOf(0.0001), Convert.Unit.ETHER).send();
+    /**
+     * Method responsible for sending a small amount of Ether (0.0001 Ether) from the admin account to a second account.
+     * This method is only needed because voter would not be able to vote because they cannot pay the gas price for the
+     * transaction.
+     *
+     * @param toAddress address of the second account where the Ether goes to
+     * @throws Exception if the transaction fails
+     */
+    public void sendTransaction(Address toAddress) throws Exception {
+        Transfer.sendFunds(web3, credentials, toAddress.toString(), BigDecimal.valueOf(0.0001), Convert.Unit.ETHER).send();
     }
 }
