ef2435e7fbed03f0a79a636cc19c1376dc2fd816
Patrick Windegger <winpab13@htl-kaindorf.ac.at>
2018-03-10 14:55:26 +0100

    AdminReader JavaDoc finished

--- src/main/java/util/AdminReader.java
+++ src/main/java/util/AdminReader.java
@@ -6,19 +6,28 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+/**
+ * Author:          Patrick Windegger
+ * Created on:
+ * Description:     Class responsible for getting the address of the admin contract
+ */
 public class AdminReader {
 
+    /**
+     * Method responsible for reading the contract.txt file where the address to the admin contract is saved.
+     *
+     * @param path: path to the contract.txt file
+     * @return address of the contract
+     * @throws Exception if the file does not exist
+     */
     public static Address getAdminContractAddress(String path) throws Exception {
         File f = new File(path + "contract.txt");
-        if(f.exists())
-        {
+        if (f.exists()) {
             BufferedReader br = new BufferedReader(new FileReader(f));
             String address = br.readLine();
             br.close();
             return new Address(address);
-        }
-        else
-        {
+        } else {
             throw new Exception("admin contract does not exist!");
         }
     }
